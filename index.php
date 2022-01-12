b3479922f2c98adc162d102f6b301c34a9f1d90b
vitoriall <vitoria2015albuquerque@gmail.com>
2022-01-12 17:53:57 -0300

    isset: para aprender php
    Aqui comecei meu exercício.

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Listagem e Paginação</title>
 </head>
 <body>
-    <?php
-
-
-
-
-     ?>
+<a href="index.php">Cadastrar</a><br>
+		<a href="listar.php">Listar</a><br>
+		<h1>Cadastrar Usuário</h1>
+		<?php
+		if(isset($_SESSION['msg'])){
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+		}
+		?>
+		<form method="POST" action="processa.php">
+			<label>Nome: </label>
+			<input type="text" name="nome" placeholder="Digite o nome completo"><br><br>
+			
+			<label>E-mail: </label>
+			<input type="email" name="email" placeholder="Digite o seu melhor e-mail"><br><br>
+			
+			<input type="submit" value="Cadastrar">
+		</form>
 </body>
 </html>
